9c9501af3911b136c03e011d23c256d37e90c212
MarcosVinicius-Mota <marcosvinicius.mota652790@gmail.com>
2023-05-06 21:59:48 -0300

    aaaaaaaaaaaaaaaaaaaaaa

--- projetoDaMinhVida/trabalho/src/main/java/com/trabalhodetc/AutomatoG3/Estados.java
+++ projetoDaMinhVida/trabalho/src/main/java/com/trabalhodetc/AutomatoG3/Estados.java
@@ -2,58 +2,57 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+
 package com.trabalhodetc.AutomatoG3;
-
-/**
- *
- * @author Victor
- */
 public class Estados {  //classe destinada à definição do autômato, seguindo padrão da estrutura xml.
-     private String id; //corresponde à numeração do estado
-     private String name; //corresponde ao nome dado ao autômato
-     private String x; //coordenada usada para localidade do estado em um plano
-     private String y; //coordenada usada para localidade do estado em um plano
+    private String id; //corresponde à numeração do estado
+    private String name; //corresponde ao nome dado ao autômato
+    private String x; //coordenada usada para localidade do estado em um plano
+    private String y; //coordenada usada para localidade do estado em um plano
+    boolean estadoFinal;
     
-     public Estados() {
-    }
+    public Estados() {
+       estadoFinal = false;
+   }
 
-    public Estados(String id, String name, String x, String y) {
-        this.id = id;
-        this.name = name;
-        this.x = x;
-        this.y = y;
-        
-    }
-    
-    public String getId() {
-        return id;
-    }
+   public Estados(String id, String name, String x, String y) {
+       this.id = id;
+       this.name = name;
+       this.x = x;
+       this.y = y;
+       estadoFinal = false;
+   }
+   
+   public String getId() {
+       return id;
+   }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+   public void setId(String id) {
+       this.id = id;
+   }
 
-    public String getName() {
-        return name;
-    }
+   public String getName() {
+       return name;
+   }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+   public void setName(String name) {
+       this.name = name;
+   }
 
-    public String getX() {
-        return x;
-    }
+   public String getX() {
+       return x;
+   }
 
-    public void setX(String x) {
-        this.x = x;
-    }
+   public void setX(String x) {
+       this.x = x;
+   }
 
-    public String getY() {
-        return y;
-    }
+   public String getY() {
+       return y;
+   }
 
-    public void setY(String y) {
-        this.y = y;
-    }
+   public void setY(String y) {
+       this.y = y;
+   }
 }
